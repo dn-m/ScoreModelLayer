ccf41f9300471f341f27b41de6b981df0eed8bfd
James <jbean46@gmail.com>
2017-01-03 15:36:39 +0100

    Add ScoreModel+Segment

---
+++ ScoreModelLayer/ScoreModel+Segment.swift
@@ -0,0 +1,15 @@
+//
+//  ScoreModel+Segment.swift
+//  ScoreModelLayer
+//
+//  Created by James Bean on 1/3/17.
+//
+//
+
+extension ScoreModel {
+    
+    public struct Segment {
+        
+        
+    }
+}
