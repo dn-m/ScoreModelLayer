192d63b51c90aade11e411f6373ff4525819bd40
James <jbean46@gmail.com>
2017-07-02 16:27:58 +0200

    Add test stub so XCTest doesn't fail

---
+++ ScoreModelLayerTests/ScoreModelTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreModelTests.swift
+//  ScoreModelLayer
+//
+//  Created by James Bean on 7/2/17.
+//
+//
+
+import XCTest
+import ScoreModelLayer
+
+class ScoreModelTests: XCTestCase {
+    
+    func testScoreModel() {
+        // TODO
+    }
+}
